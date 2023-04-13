737bf680556ccd0a0e1b83e8035f0d7ac3bf542e
etobaza <rblxxmartin@gmail.com>
2023-04-14 01:43:54 +0600

    Initial commit

--- Lab10/api/urls.py
+++ Lab10/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CompanyList, CompanyDetail, CompanyVacancyList, VacancyList, VacancyDetail, TopTenVacancies
+from .views import CompanyList, CompanyDetail, CompanyVacancyList, VacancyList, VacancyDetail, TopTenVacancies, CompanyCreate, VacancyCreate, CompanyUpdate, VacancyUpdate, CompanyDelete, VacancyDelete
 
 urlpatterns = [
     path('companies/', CompanyList.as_view(), name='company-list'),
@@ -11,5 +11,7 @@ urlpatterns = [
     path('vacancies/', VacancyList.as_view(), name='vacancy-list'),
     path('vacancies/create/', VacancyCreate.as_view(), name='vacancy-create'),
     path('vacancies/<int:pk>/', VacancyDetail.as_view(), name='vacancy-detail'),
-    path('vacancies/<int:pk>/update/', VacancyUpdate.as_view(), name
+    path('vacancies/<int:pk>/update/', VacancyUpdate.as_view(), name='vacancy-update'),
+    path('vacancies/<int:pk>/delete/', VacancyDelete.as_view(), name='vacancy-delete'),
+    path('vacancies/top_ten/', TopTenVacancies.as_view(), name='top-ten-vacancies'),
 ]
